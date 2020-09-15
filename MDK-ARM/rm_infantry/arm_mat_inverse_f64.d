c2f8e903a2ae002920765cf50d69333f9c0cfe6e
Murphy41 <48173064+Murphy41@users.noreply.github.com>
2020-09-15 12:22:23 +1000

    MDK-ARM

---
+++ MDK-ARM/rm_infantry/arm_mat_inverse_f64.d
@@ -0,0 +1,10 @@
+rm_infantry\arm_mat_inverse_f64.o: ..\Drivers\CMSIS\Device\arm_mat_inverse_f64.c
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/arm_math.h
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/core_cm4.h
+rm_infantry\arm_mat_inverse_f64.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+rm_infantry\arm_mat_inverse_f64.o: ../Drivers/CMSIS/Include/core_cmSimd.h
+rm_infantry\arm_mat_inverse_f64.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+rm_infantry\arm_mat_inverse_f64.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
