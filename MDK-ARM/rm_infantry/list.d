04d7994815bf762c3137eab4a57a6dbf23552870
Murphy41 <48173064+Murphy41@users.noreply.github.com>
2020-09-15 12:30:59 +1000

    MDK-ARM

---
+++ MDK-ARM/rm_infantry/list.d
@@ -0,0 +1,13 @@
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/list.c
+rm_infantry\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+rm_infantry\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+rm_infantry\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+rm_infantry\list.o: ../Inc/FreeRTOSConfig.h
+rm_infantry\list.o: ../Inc/main.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+rm_infantry\list.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
---
+++ MDK-ARM/rm_infantry/port.d
@@ -0,0 +1,13 @@
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/port.c
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+rm_infantry\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+rm_infantry\port.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+rm_infantry\port.o: ../Inc/FreeRTOSConfig.h
+rm_infantry\port.o: ../Inc/main.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+rm_infantry\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
